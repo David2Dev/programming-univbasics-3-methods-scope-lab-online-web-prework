e8345654a6536b07c7eb6a8993bea51b3baedfd8
David Jermaine Whitehead <david_whitehead_234@yahoo.com>
2019-07-09 22:11:03 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,7 +1,9 @@
 def mario
-  status = "It's-a me, Mario!"
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
+  puts phrase
 end
 
 def toadstool
-  puts status = "Thank You Mario! But Our Princess Is In Another Castle!"
+  puts status
 end
